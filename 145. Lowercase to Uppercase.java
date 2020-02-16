da2ce5198c4c3a36a61df0e61d18dfabf5a3cf30
leaonhay <59476136+leaonhay@users.noreply.github.com>
2020-02-16 12:05:47 +0800

    Create 145. Lowercase to Uppercase.java
    Naive

---
+++ 145. Lowercase to Uppercase.java
@@ -0,0 +1,23 @@
+Convert a lowercase character to uppercase.
+
+样例
+Example 1:
+
+Input: 'a'
+Output: 'A'
+Example 2:
+
+Input: 'b'
+Output: 'B'
+
+public class Solution {
+    /**
+     * @param character: a character
+     * @return: a character
+     */
+    public char lowercaseToUppercase(char character) {
+        // write your code here
+        character -= 32;
+        return character;
+    }
+}
